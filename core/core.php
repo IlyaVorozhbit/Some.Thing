b548d6e1720bd54c9d4f6f27cfce58d6ddec573f
iNull <ilya@vorozhbit.su>
2015-11-29 18:53:50 +0300

    Обновление Ядра (мини-роутер)
    - определение вызванного модуля
    - определение вызванного метода

--- core/core.php
+++ core/core.php
@@ -1,15 +1,35 @@
 <?php
 
     class Core {
-        
+
+        private $module;
+        private $mode;
+
         public function __construct(){
+
             if(debug)
                 echo 'Core initializing';
+
+            $this->module = $this->defineModule();
+            $this->mode = $this->defineMode();
+
         }
 
-        public function __destruct(){
-
+        public function defineModule(){
+            return isset($_GET['module'])? $_GET['module'] : 'news';
         }
 
+        public function defineMode(){
+            return isset($_GET['mode'])? $_GET['mode'] : 'index';
+        }
+
+        // функции ниже потому что доступ до них - приватный
+        public function module(){
+            return $this->module;
+        }
+
+        public function mode(){
+            return $this->mode;
+        }
 
     }
