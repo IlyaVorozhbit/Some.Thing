8ecd8dd625fc015b38752644130384b5cbb7938c
iNull <ilya@vorozhbit.su>
2015-11-30 09:17:44 +0300

    layout update
    title & favicon adding

--- design/layout.php
+++ design/layout.php
@@ -2,6 +2,8 @@
     <head>
         <link rel="stylesheet" type="text/css" href="http://vay.im/static/css/libraries/bootstrap.css" media="screen, projection" />
         <link rel="stylesheet" type="text/css" href="/static/css/main.css" media="screen, projection" />
+        <link id="favicon" rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <title>MCTop: Первый. Честный</title>
     </head>
 
     <body>
