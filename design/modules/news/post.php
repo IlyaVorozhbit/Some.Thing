257cd3d608104b57cc75fa2a72d03326ebe82879
iNull <ilya@vorozhbit.su>
2015-11-30 09:16:06 +0300

    Исправление "вьюшки" новостей
    - убрали hr

--- design/modules/news/post.php
+++ design/modules/news/post.php
@@ -6,8 +6,10 @@
     <li class="active"><?php echo $post['title'];?></li>
 </ol>
 
-<h1><?php echo $post['title'];?></h1><hr>
+<h1><?php echo $post['title'];?></h1>
+
 <?php echo $post['full']?><br>
+
 ID Авторa: <?php echo $post['author_id']?><br>
 Создано: в <?php echo date('H:i:s, d/y', strtotime($post['created_at']))?><br>
 <hr>
