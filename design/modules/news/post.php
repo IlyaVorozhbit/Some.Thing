400287a020ac86599fd6400b758c1824e3ce2280
iNull <ilya@vorozhbit.su>
2015-11-30 12:56:49 +0300

    VK Likes & Comments

--- design/modules/news/post.php
+++ design/modules/news/post.php
@@ -14,3 +14,26 @@ ID Авторa: <?php echo $post['author_id']?><br>
 Создано: в <?php echo date('H:i:s, d/y', strtotime($post['created_at']))?><br>
 <hr>
 MCTop, будь с нами.
+<br><br>
+
+<h2>VK</h2>
+
+<div>
+    <script type="text/javascript" src="//vk.com/js/api/openapi.js?121"></script>
+
+    <script type="text/javascript">
+        VK.init({apiId: 4585317, onlyWidgets: true});
+    </script>
+
+    <div id="vk_comments"></div>
+    <script type="text/javascript">
+        VK.Widgets.Comments("vk_comments", {limit: 5, width: "665", attach: false});
+    </script>
+    <hr>
+
+    <!-- Put this div tag to the place, where the Like block will be -->
+    <div id="vk_like"></div>
+    <script type="text/javascript">
+        VK.Widgets.Like("vk_like", {type: "button"});
+    </script>
+</div>
