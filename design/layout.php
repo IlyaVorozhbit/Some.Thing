ffd9bcf578e7fa44a4cf805a53687660444ec43e
iNull <ilya@vorozhbit.su>
2015-11-30 11:42:20 +0300

    Google Analytics

--- design/layout.php
+++ design/layout.php
@@ -41,6 +41,18 @@
             <!--LiveInternet counter--><script type="text/javascript">document.write("<a href='//www.liveinternet.ru/click' target=_blank><img src='//counter.yadro.ru/hit?t12.1;r" + escape(top.document.referrer) + ((typeof(screen)=="undefined")?"":";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?screen.colorDepth:screen.pixelDepth)) + ";u" + escape(document.URL) +";h"+escape(document.title.substring(0,80)) +  ";" + Math.random() + "' border=0 width=88 height=31 alt='' title='LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня'><\/a>")</script><!--/LiveInternet-->
 
             <div class="copyright">© <a href="/pages?mode=about"> MCTop.Team</a>. <br><a target="_blank" href="/static/images/mine_serv_screen.png">30.01.2011</a> - настоящее время. </div>
+
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-51962577-1', 'auto');
+                ga('send', 'pageview');
+
+            </script>
+
         </div>
 
     </body>
