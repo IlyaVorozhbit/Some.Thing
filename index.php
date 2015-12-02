a9f329f88ca6a90c0d64cb91118365b1d15d5035
TheAndrey <andrey9506@yandex.ru>
2015-12-02 11:24:17 +0200

    Вывод ошибок стоит отключить.
    Он нужен только при разработке. Посетителям сайта видеть их не нужно.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     define('IN_DEV', true); //if debug is on, write logs to page;
     define('ROOT_DIR', __DIR__);
 
+	@ini_set("display_errors", IN_DEV); // Вывод ошибок в релизе стоит отключить из соображений безопасности. Они будут писаться в лог.
+
     require_once('core/init.php');
 
     $initializator = new Initializator();
