4f279f15b22d4fdc64cf37eb7113848e762a69b2
Ilya Vorozhbit <ilya@mctop.im>
2015-12-07 20:03:42 +0300

    lastupdates

--- design/themes/default/modules/pages/about.php
+++ design/themes/default/modules/pages/about.php
@@ -3,31 +3,46 @@
     <h1>MCTop: О сайте <g:plusone></g:plusone></h1>
 </div>
 
-<p><a href="/">MCTop</a> - первый в России рейтинг серверов игры Minecraft.<br><br>
+<div class="about">
+    <p><a href="/">MCTop</a> - первый в России рейтинг серверов игры Minecraft.<br><br>
 
-С момента своего открытия 1 февраля 2011, проект продолжает удерживать лидирующие позиции среди своих аналогов.</p>
+        С момента своего открытия 1 февраля 2011, проект продолжает удерживать лидирующие позиции среди своих аналогов.</p>
 
-<p>Администрация и команда разработчиков постоянно трудятся над улучшением качества и расширением функционала рейтинга.  </p>
+    <p>Администрация и команда разработчиков постоянно трудятся над улучшением качества и расширением функционала рейтинга.  </p>
 
-<p>Регистрируясь у нас, Вы получаете полный контроль над собственным сервером, существенно увеличивая его посещаемость.
-    Оперативный отдел техподдержки быстро решит все возникшие при работе с сервисом проблемы и предотвратит их дальнейшее появление. </p>
+    <p>Регистрируясь у нас, Вы получаете полный контроль над собственным сервером, существенно увеличивая его посещаемость.
+        Оперативный отдел техподдержки быстро решит все возникшие при работе с сервисом проблемы и предотвратит их дальнейшее появление. </p>
 
-<p>Одним из главных достоинств сайта является минимальная вероятность накрутки голосов, а в связи с этим - на первых местах рейтинга действительно достойные сервера. </p>
+    <p>Одним из главных достоинств сайта является минимальная вероятность накрутки голосов, а в связи с этим - на первых местах рейтинга действительно достойные сервера. </p>
 
-<p>MCTop - с нами удобно!</p>
+    <p>MCTop - с нами удобно!</p>
+</div>
 
-<center><img src="/static/images/mctop_old.jpg" /></center>
+
+<center><img style="max-height: 100px;" src="/static/images/mctop_old.jpg" /></center>
 <br>
-<p class="alert alert-info">PS. MCTop читается как [Эм-Ка-Топ]. Постольку поскольку игра называется Майнкрафт, а не Майн(простите)Срафт, МайнСирафт и т.п.</p>
 
-<h2>Мы в Сети</h2><hr>
+<h2 style="border-bottom: 1px solid #cacaca;">Мы в Сети</h2>
 
-<p>ВКонтакте: <a href="http://vk.com/mctop.onvay" target="_blank">http://vk.com/mctop.official</a></p>
+    <a href="http://vk.com/mctop.onvay" target="_blank">
+        <span style="background-color: #5F83AA; color: #fff; padding: 4px; font-size: 13px; border-radius: 4px;">Мы ВКонтакте</span>
+    </a>
 
-<p>Google+: <a href="https://plus.google.com/b/114997155561494426361/114997155561494426361" target="_blank">https://plus.google.com/b/114997155561494426361/114997155561494426361</a></p>
+    <a href="https://plus.google.com/b/114997155561494426361/114997155561494426361" target="_blank">
+        <span style="background-color: #FD0000; color: #fff; padding: 4px; font-size: 13px; border-radius: 4px;">Мы в Google +</span>
+    </a>
 
-<p>YouTube: <a href="http://www.youtube.com/channel/UCQjrtSwHWl0rnsMG8-xy8oA" target="_blank">http://www.youtube.com/channel/UCQjrtSwHWl0rnsMG8-xy8oA</a></p>
+    <a href="http://www.youtube.com/channel/UCQjrtSwHWl0rnsMG8-xy8oA" target="_blank">
+        <span style="background-color: #FD0000; color: #fff; padding: 4px; font-size: 13px; border-radius: 4px;">Мы на YouTube</span>
+    </a>
 
-<p>Так же, Вы можете присоединиться к чату разработчиков MCTop на Gitter: <a target="_blank" href="https://gitter.im/IlyaVorozhbit/some.thing">Зайти на чай</a> </p>
+    <a style="position: relative; top:4px;" href="https://gitter.im/IlyaVorozhbit/Some.Thing?utm_source=badge&amp;utm_medium=badge&amp;utm_campaign=pr-badge">
+        <img src="https://camo.githubusercontent.com/da2edb525cde1455a622c58c0effc3a90b9a181c/68747470733a2f2f6261646765732e6769747465722e696d2f4a6f696e253230436861742e737667" alt="Gitter" data-canonical-src="https://badges.gitter.im/Join%20Chat.svg" style="max-width:100%;">
+    </a>
 
+<h2 style="border-bottom: 1px solid #cacaca;">Статистика MCTop</h2>
 
+<div class="counters">
+    <!-- Yandex.Metrika informer --><a href="https://metrika.yandex.ru/stat/?id=4081210&amp;from=informer"target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/4081210/3_0_FFFF20FF_FFFF00FF_0_pageviews"style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:4081210,lang:'ru'});return false}catch(e){}" /></a><!-- /Yandex.Metrika informer --> <!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter4081210 = new Ya.Metrika({ id:4081210, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/4081210" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+    <!--LiveInternet counter--><script type="text/javascript">document.write("<a href='//www.liveinternet.ru/click' target=_blank><img src='//counter.yadro.ru/hit?t12.1;r" + escape(top.document.referrer) + ((typeof(screen)=="undefined")?"":";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?screen.colorDepth:screen.pixelDepth)) + ";u" + escape(document.URL) +";h"+escape(document.title.substring(0,80)) +  ";" + Math.random() + "' border=0 width=88 height=31 alt='' title='LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня'><\/a>")</script><!--/LiveInternet-->
+</div>
--- design/themes/default/modules/pages/anniversary.php
+++ design/themes/default/modules/pages/anniversary.php
@@ -1,44 +1,56 @@
-<p class="MsoNormal">Дорогие друзья!</p>
-<p class="MsoNormal">Ровно год назад, 1 февраля 2011 года, был открыт первый в России рейтинг серверов <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Minecraft</span></strong>.</p>
-<p class="MsoNormal">Амбициозный проект, которому было суждено показать на собственном примере, насколько сильно выросло сообщество этой независимой игры, какую популярность снискал сетевой режим и сколько людей решило создать собственный сервер.</p>
-<p class="MsoNormal"><span lang="EN-US" style="mso-ansi-language: EN-US;">MCTop</span> вырос практически из ничего, у его авторов не было ни малейшего опыта в разработке сайтов. Фактически нами двигало лишь идея и сильнейшее желание довести дело до конца.</p>
-<p class="MsoNormal">Теперь пришло время вернуться назад и посмотреть с чего же всё начиналось:</p>
-<ol style="margin-top: 0cm;" start="1" type="1">
-    <li class="MsoNormal" style="mso-list: l0 level1 lfo1;">
-        <h3><strong>Первый этап. Зарождение идеи. <span lang="EN-US" style="mso-ansi-language: EN-US;">UCoZ</span>.</strong></h3>
-    </li>
-</ol>
-<p><img style="width: 500px; height: 473px;" src="http://img.gameru.net/img/bd463.jpg" alt="" width="500" height="473" /></p>
-<p>&nbsp;</p>
-<p class="MsoNormal">Как и все начинающие &ldquo;сайтоделы&rdquo;, свою идею мы решили реализовать вполне очевидным образом &ndash; воспользовавшись услугами сервиса <span lang="EN-US" style="mso-ansi-language: EN-US;">UCoZ</span>.</p>
-<p class="MsoNormal">Фактически, это был стандартный сайт с чуть-чуть измененным шаблоном и новым меню.</p>
-<p class="MsoNormal">Сам же рейтинг представлял собой отдельную страницу, где голосование проходило путём выставления звёздочек (от 1 до 5).</p>
-<p class="MsoNormal">Никакой защиты не было и в помине &ndash; накрутка голосов на том этапе нас совершенно не волновала.</p>
-<ol style="margin-top: 0cm;" start="2" type="1">
-    <li class="MsoNormal" style="mso-list: l0 level1 lfo1;">
-        <h3><strong>Второй этап. Переход на платный хостинг и новый движок.</strong></h3>
-    </li>
-</ol>
-<p><img style="width: 600px; height: 340px;" src="http://img.gameru.net/img/c2c68.jpg" alt="" width="600" height="340" /> <img style="width: 600px; height: 339px;" src="http://img.gameru.net/img/da9c7.jpg" alt="" width="600" height="339" /></p>
-<p class="MsoNormal">Поняв, что идея себя оправдала, мы приняли решение о переносе рейтинга на платный хостинг. Собственно, доменное имя <span lang="EN-US" style="mso-ansi-language: EN-US;">MCTop</span>.<span lang="EN-US" style="mso-ansi-language: EN-US;">su</span> появилось именно сейчас.</p>
-<p class="MsoNormal">Перед нами встала задача найти удобный и бесплатный движок, который бы соответствовал нашим требованиям. По многим причинам, выбор пал на Aardvark Topsites.</p>
-<p class="MsoNormal">На этом этапе проект развивался молниеносными темпами, популярность росла, появились первые конкуренты.</p>
-<p class="MsoNormal">Тут то и начали выявляться первые серьёзные проблемы. Та же накрутка съела много нервных клеток всем членам нашей команды (в особенности, программистам).</p>
-<p class="MsoNormal">По правде говоря, именно из-за неё мы на некоторое время впали в состояние забвения, так как решить проблему должным образом не получалось.</p>
-<ol style="margin-top: 0cm;" start="3" type="1">
-    <li class="MsoNormal" style="mso-list: l0 level1 lfo1;">
-        <h3><strong>Третий этап. Новый движок. <span lang="EN-US" style="mso-ansi-language: EN-US;">MCTop 2.0</span></strong></h3>
-    </li>
-</ol>
-<p><strong><span lang="EN-US" style="mso-ansi-language: EN-US;"> <img style="width: 600px; height: 357px;" src="http://img.gameru.net/img/61221.jpg" alt="" width="600" height="357" /></span></strong></p>
-<p class="MsoNormal">С появлением <span lang="EN-US" style="mso-ansi-language: EN-US;">MCTop</span>2.0, мы полностью пересмотрели нашу политику &ndash; для искоренения накрутки был выбран метод голосования через социальную сеть ВКонтакте.</p>
-<p class="MsoNormal">Возможно, он несколько не честный по отношению к людям, кто не регистрируется в подобных проектах из принципа, но мы сознательно пошли на этот шаг.</p>
-<p class="MsoNormal">Вдобавок ко всему, рейтинг получил строгий минималистический дизайн, выполненный в спокойной цветовой гамме &ndash; после чёрного выглядело очень свежо.</p>
-<p class="MsoNormal">Дабы не писать стену текста, мы дали очень краткое описание всего того, что произошло в нашей жизни за этот год.</p>
-<p class="MsoNormal">За кадром остались ссоры, междоусобные разборки, отражение <span lang="EN-US" style="mso-ansi-language: EN-US;">DDoS</span> атак и многое другое.</p>
-<p class="MsoNormal">Самое главное, что мы хотим сказать Вам, нашим дорогим пользователям, это спасибо.</p>
-<p class="MsoNormal">Спасибо за то, что всё это время были с нами, за поддержку и за помощь в развитии и улучшении качества предоставляемых услуг.</p>
-<p class="MsoNormal">Вы помогли нам доказать всему миру, что <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Minecraft</span> </strong>теперь такая же большая серьёзная игра, как <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">World</span> </strong><strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">of</span> </strong><strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Warcraft</span></strong> и <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Lineage</span> 2</strong>, и что ему тоже необходим свой рейтинг серверов.</p>
-<p class="MsoNormal">Впереди новый год, и мы продолжим улучшение и совершенствование проекта.</p>
-<p class="MsoNormal">Оставайтесь с нами!</p>
-<h3 align="right">С уважением, администрация первого в России рейтинга серверов</h3>
+<h2>Сообщение от 1 февраля 2012 года</h2>
+
+<div class="anniversary">
+
+    <p>Дорогие друзья!</p>
+    <p>Ровно год назад, 1 февраля 2011 года, был открыт первый в России рейтинг серверов <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Minecraft</span></strong>.</p>
+    <p>Амбициозный проект, которому было суждено показать на собственном примере, насколько сильно выросло сообщество этой независимой игры, какую популярность снискал сетевой режим и сколько людей решило создать собственный сервер.</p>
+    <p>MCTop вырос практически из ничего, у его авторов не было ни малейшего опыта в разработке сайтов. Фактически нами двигало лишь идея и сильнейшее желание довести дело до конца.</p>
+    <p>Теперь пришло время вернуться назад и посмотреть с чего же всё начиналось.</p>
+
+    <h2>Первый этап. Зарождение идеи. <span lang="EN-US" style="mso-ansi-language: EN-US;">UCoZ</span></h2>
+
+    <a href="http://img.gameru.net/img/bd463.jpg" target="_blank">
+        <img style="max-height: 100px;" src="http://img.gameru.net/img/bd463.jpg" />
+    </a>
+
+    <p>Как и все начинающие "сайтоделы", свою идею мы решили реализовать вполне очевидным образом &ndash; воспользовавшись услугами сервиса <span lang="EN-US" style="mso-ansi-language: EN-US;">UCoZ</span>.</p>
+    <p>Фактически, это был стандартный сайт с чуть-чуть измененным шаблоном и новым меню.</p>
+    <p>Сам же рейтинг представлял собой отдельную страницу, где голосование проходило путём выставления звёздочек (от 1 до 5).</p>
+    <p>Никакой защиты не было и в помине &ndash; накрутка голосов на том этапе нас совершенно не волновала.</p>
+
+    <h2>Второй этап. Переход на платный хостинг и новый движок</h2>
+
+    <a href="http://img.gameru.net/img/c2c68.jpg" target="_blank">
+        <img style="max-height: 100px;" src="http://img.gameru.net/img/c2c68.jpg"/>
+    </a>
+
+    <a href="http://img.gameru.net/img/da9c7.jpg" target="_blank">
+        <img style="max-height: 100px;" src="http://img.gameru.net/img/da9c7.jpg"/>
+    </a>
+
+    <p>Поняв, что идея себя оправдала, мы приняли решение о переносе рейтинга на платный хостинг. Собственно, доменное имя <span lang="EN-US" style="mso-ansi-language: EN-US;">MCTop</span>.<span lang="EN-US" style="mso-ansi-language: EN-US;">su</span> появилось именно сейчас.</p>
+    <p>Перед нами встала задача найти удобный и бесплатный движок, который бы соответствовал нашим требованиям. По многим причинам, выбор пал на Aardvark Topsites.</p>
+    <p>На этом этапе проект развивался молниеносными темпами, популярность росла, появились первые конкуренты.</p>
+    <p>Тут то и начали выявляться первые серьёзные проблемы. Та же накрутка съела много нервных клеток всем членам нашей команды (в особенности, программистам).</p>
+    <p>По правде говоря, именно из-за неё мы на некоторое время впали в состояние забвения, так как решить проблему должным образом не получалось.</p>
+
+    <h2>Третий этап. Новый движок. MCTop 2.0</h2>
+
+    <a href="http://img.gameru.net/img/61221.jpg" target="_blank">
+        <img style="max-height: 100px;" src="http://img.gameru.net/img/61221.jpg"/>
+    </a>
+
+    <p>С появлением MCTop 2.0, мы полностью пересмотрели нашу политику - для искоренения <b>накрутки</b> был выбран метод голосования через социальную сеть ВКонтакте.</p>
+    <p>Возможно, он несколько не честный по отношению к людям, кто не регистрируется в подобных проектах из принципа, но мы сознательно пошли на этот шаг.</p>
+    <p>Вдобавок ко всему, рейтинг получил строгий минималистический дизайн, выполненный в спокойной цветовой гамме &ndash; после чёрного выглядело очень свежо.</p>
+    <p>Дабы не писать стену текста, мы дали очень краткое описание всего того, что произошло в нашей жизни за этот год.</p>
+    <p>За кадром остались ссоры, междоусобные разборки, отражение <span lang="EN-US" style="mso-ansi-language: EN-US;">DDoS</span> атак и многое другое.</p>
+    <p>Самое главное, что мы хотим сказать Вам, нашим дорогим пользователям, это спасибо.</p>
+    <p>Спасибо за то, что всё это время были с нами, за поддержку и за помощь в развитии и улучшении качества предоставляемых услуг.</p>
+    <p>Вы помогли нам доказать всему миру, что <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Minecraft</span> </strong>теперь такая же большая серьёзная игра, как <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">World</span> </strong><strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">of</span> </strong><strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Warcraft</span></strong> и <strong style="mso-bidi-font-weight: normal;"><span lang="EN-US" style="mso-ansi-language: EN-US;">Lineage</span> 2</strong>, и что ему тоже необходим свой рейтинг серверов.</p>
+    <p>Впереди новый год, и мы продолжим улучшение и совершенствование проекта.</p>
+    <p>Оставайтесь с нами!</p>
+    <h3 align="right">С уважением, администрация первого в России рейтинга серверов</h3>
+</div>
+
