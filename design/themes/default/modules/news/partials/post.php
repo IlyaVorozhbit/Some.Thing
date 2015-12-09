70ed198905ce0dd6c2877f449b96d2fb77ea45ef
Илья <ilya@vorozhbit.su>
2015-12-09 12:52:18 +0300

    Срочная починка новостей дизайна.

--- design/themes/default/modules/news/partials/post.php
+++ design/themes/default/modules/news/partials/post.php
@@ -1,6 +1,6 @@
 <div class="news-block">
     <div class="news-info"><a href="/news/post/<?php echo $post['id'];?>"><?php echo $post['title'];?></a> | Дата: <?php echo $post['created_at'];?> | Автор: <?php echo $post['author_id'];?> | Просмотров: <?php echo $post['views']?></div>
-    <div class="news-content">
+    <div class="news-content" style="background-color: #06525E;">
         <p>
             <?php echo $post['preview'];?>
         </p>
