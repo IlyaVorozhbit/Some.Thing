ef3b904d617993b8e007086e8d951c7a17b0f4ad
Илья <ilya@mctop.me>
2016-06-25 07:04:34 +0300

    MCTop to Vay

--- design/themes/default/oldschool.php
+++ design/themes/default/oldschool.php
@@ -46,7 +46,7 @@
             </div>
 
             <div class="top-menu">
-                <a href="/news/post/15" title="Конкурс на разработку дизайна">MCTop.Дизайн</a> |
+                <a href="/news/post/15" title="Конкурс на разработку дизайна">Главная</a> |
                 <a href="/servers" title="Список серверов Minecraft от MCTop.me">Сервера Minecraft</a> |
                 <!--<a href="/social" title="Вход на Mine-Russia.Social">Войти</a>-->
                 <a href="/pages?mode=register" title="Добавление своего сервера на MCTop.me">Регистрация</a> |
@@ -91,15 +91,10 @@
                         <li><a href="/pages?mode=developers" title="Разработчикам и всем тем, кому интересно развитие MCTop">Разработчикам</a></li>
                         <li><a href="/pages?mode=rules">Правила</a></li>
                         <li><a href="/pages?mode=sitemap">Карта сайта</a></li>
-                        <li><a target="_blank" href="https://vk.com/mctop.official">Мы ВКонтакте</a></li>
+                        <li><a target="_blank" href="https://vk.com/vay_im">Мы ВКонтакте</a></li>
                     </ul>
                 </div>
                 <div class="footer__row">
-                    <div class="copyright">© <a title="Появление первой версии проекта по адресу mine-serv.do.am" target="_blank" href="/static/images/mine_serv_screen.png">2011</a> - <?= date('Y') ?>. <a title="Первый. Верный" href="/pages?mode=about"> MCTop.Team</a>
-                        <div class="engine" <?php if ($initializator->core->getAction() != '.'): ?>style="display:none;"<?php endif ?>>
-                            <a style="font-size: 10px; text-decoration: underline;" href="https://github.com/IlyaVorozhbit/Some.Thing">Версия Some.Thing: <?php echo $version; ?></a>
-                        </div>
-                    </div>
                     <div class="counters" <?php if ($initializator->core->getAction() != '.'): ?>style="display:none;"<?php endif ?>>
                         <!-- Yandex.Metrika informer --><a href="https://metrika.yandex.ru/stat/?id=4081210&amp;from=informer"target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/4081210/3_0_FFFF20FF_FFFF00FF_0_pageviews"style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:4081210,lang:'ru'});return false}catch(e){}" /></a><!-- /Yandex.Metrika informer --> <!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter4081210 = new Ya.Metrika({ id:4081210, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/4081210" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
                         <!--LiveInternet counter--><script type="text/javascript">document.write("<a href='//www.liveinternet.ru/click' target=_blank><img src='//counter.yadro.ru/hit?t12.1;r" + escape(top.document.referrer) + ((typeof(screen)=="undefined")?"":";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?screen.colorDepth:screen.pixelDepth)) + ";u" + escape(document.URL) +";h"+escape(document.title.substring(0,80)) +  ";" + Math.random() + "' border=0 width=88 height=31 alt='' title='LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня'><\/a>")</script><!--/LiveInternet-->
